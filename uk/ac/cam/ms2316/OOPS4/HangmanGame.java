c1a29b63a7885bbc9dbd98e539663b3f5f9a7ad2
Mihails Smolins <ms2316@cam.ac.uk>
2016-01-13 23:06:12 +0000

    initial commit

--- uk/ac/cam/ms2316/OOPS4/HangmanGame.java
+++ uk/ac/cam/ms2316/OOPS4/HangmanGame.java
@@ -1,19 +1,92 @@
 package uk.ac.cam.ms2316.OOPS4;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 class HangmanGame {
   
+	//Fields
+	private String[] dictionary;
+	private String word;
+	private int movesMade;
+	private int movesAllowed;
+	private ArrayList<Character> correctLetters;
+	private ArrayList<Character> incorrectLetters;
+	private Scanner scn = new Scanner(System.in); // for user input
   
-  private String[] dictionary;
-  private String word;
-  private int movesMade;
-  private int movesAllowed;
+	//Constructor
+	public HangmanGame() {
+		//dictionary = FileIO.getWords();
+		movesAllowed = 6; //depends on drawing
+	}
   
+	/*
+		Public methods
+	*/
   
-  public HangmanGame() {
-    //dictionary = FileIO.getWords();
-    movesAllowed = 6; //depends on drawing
-  }
+	public void playGame() {
+		//initialization of the game
+		initSecretWord();
+      
+		//game
 
+		while ( !gameOver() ) {
+			//print current Hangman
+			printHangman();
+		
+		//Print correct letters guessed
+		for (int i = 0; i < correctLetters.size(); i++) 
+			System.out.print(correctLetters.get(i) + " ");
+		System.out.println("\n");
+      
+		//Print incorrect letters  guessed
+		System.out.print("Wrong letters: ");
+		for (int i = 0; i < this.incorrectLetters.size(); i++)
+			System.out.print(this.incorrectLetters.get(i) + " ");
+			
+		//Prompt and read the next guess
+		System.out.print("\nEnter a lower-case letter as your guess: ");
+		String guess = stdin.nextLine();
+			
+		//Process the next guess
+		handleGuess(guess.charAt(0));
+		
+		}
+		
+		
+		if (gameWon()) {
+			System.out.println("Congratulations, you won!");
+		} else {
+			System.out.println("Sorry, you lost.");
+			printHangman();
+		}
+		
+	}
+  
+  /*
+    Private methods
+  */
+	private String initSecretWord() {
+		Random randIndex = new Random();
+		int index = randIndex.nextInt(dictionary.length);
+		word = dictionary[index];
+		
+		correctLetters = new ArrayList<Character>();
+		for (int i = 0; i < word.length(); i++)
+		correctLetters.add('_');
+		incorrectLetters = new ArrayList<Character>();
+	}
+  
+	private boolean gameOver() {
+		//ToComplete
+		return false;
+	}
+  
+	private boolean gameWon() {
+		//ToComplete
+		return false;
+	}
+  
 
 }
 
