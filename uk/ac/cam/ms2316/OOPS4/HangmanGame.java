2365bc3e958ab3c3400b03c1c67ae915f9be3f4a
Mihails Smolins <ms2316@cam.ac.uk>
2016-01-13 22:39:33 +0000

    initial commit

---
+++ uk/ac/cam/ms2316/OOPS4/HangmanGame.java
@@ -0,0 +1,19 @@
+package uk.ac.cam.ms2316.OOPS4;
+
+class HangmanGame {
+  
+  
+  private String[] dictionary;
+  private String word;
+  private int movesMade;
+  private int movesAllowed;
+  
+  
+  public HangmanGame() {
+    //dictionary = FileIO.getWords();
+    movesAllowed = 6; //depends on drawing
+  }
+
+
+}
+
